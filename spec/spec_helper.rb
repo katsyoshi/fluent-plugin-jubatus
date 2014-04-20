babed78c435c822aed8627ab5d3f7471dd79e642
MATSUMOTO Katsuyoshi <matsumoto.katsuyoshi+github@gmail.com>
2014-04-20 22:48:38 +0900

    add coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,9 @@
+require 'simplecov'
+
+SimpleCov.start do
+  add_filter 'spec'
+end
+
 $:.unshift File.expand_path(File.join(__dir__, '..', 'lib'))
 require 'fluent/test'
 require 'fluent/plugin/out_jubatus'
